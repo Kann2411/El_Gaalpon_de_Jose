97a4a1526026a7acb714504487e18184371fdae5
Kristian Ferrin <129319448+Kann2411@users.noreply.github.com>
2024-10-14 15:37:44 -0500

    upload-Module/Almacenamiento-de-archivos

--- Backend/project_backend/src/modules/file-upload/file-upload.repository.ts
+++ Backend/project_backend/src/modules/file-upload/file-upload.repository.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FileRepository {}
+export class FileRepository {
+  async uploadImgProfile(id: string, file: Express.Multer.File) {}
+}
--- Backend/project_backend/src/modules/file-upload/file-upload.service.ts
+++ Backend/project_backend/src/modules/file-upload/file-upload.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { FileRepository } from './file-upload.repository';
 
 @Injectable()
-export class FileService {}
+export class FileService {
+  constructor(private readonly fileRepository: FileRepository) {}
+
+  uploadImgProfile(id: string, file: Express.Multer.File) {
+    return this.fileRepository.uploadImgProfile(id, file);
+  }
+}
