ff900149cd36b5a85d63268b2fa69c5fb592d674
Kristian Ferrin <129319448+Kann2411@users.noreply.github.com>
2024-10-15 11:01:05 -0500

    config/cloudinary

--- Backend/src/modules/file-upload/file-upload.module.ts
+++ Backend/src/modules/file-upload/file-upload.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { FileUploadController } from './file-upload.controller';
 import { FileService } from './file-upload.service';
 import { FileRepository } from './file-upload.repository';
+import { cloudinaryConfig } from 'src/config/cludinary';
 
 @Module({
   imports: [],
   controllers: [FileUploadController],
-  providers: [FileService, FileRepository],
+  providers: [FileService, FileRepository, cloudinaryConfig],
 })
 export class FileUploadModule {}
