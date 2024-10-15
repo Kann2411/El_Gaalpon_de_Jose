e97e4889024c6dc848ee0160f1381aeafe95dbfa
Joaquin Carretero <156371373+JoaquinCarretero@users.noreply.github.com>
2024-10-14 21:50:30 -0300

    Implementacion de la barra de navegacion

---
+++ Frontend/src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <h1>
+      Esta es la vista de contacto
+    </h1>
+  )
+}
+
+export default Contact
---
+++ Frontend/src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h1>
+      Vista de Home
+    </h1>
+  )
+}
+
+export default Home
