e06942ca4cbba5efcae753577f05580c6263a1a7
jhonatanjcdg <j3172224877@gmail.com>
2024-10-15 09:51:47 -0500

    Agrego middleware Logger

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerGlobal } from './modules/middlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(LoggerGlobal)
+
   await app.listen(3000);
 }
 bootstrap();
