30253ba9cdda9f27314b25d36ba35ac3e6b3354d
lucasmzd <ilucasmzd@gmail.com>
2024-10-17 17:39:22 -0300

    Carousel change

--- Frontend/src/app/home/page.tsx
+++ Frontend/src/app/home/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import CarouselComponent from '@/components/Carousel/Carousel'
 
 const Home = () => {
   return (
     <div>
-      <CarouselComponent />
+      <h1>Home</h1>
     </div>
   )
 }
