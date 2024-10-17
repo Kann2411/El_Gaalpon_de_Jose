ccd200f0fe4286ea32bec65949f7364e05c56a64
lucasmzd <ilucasmzd@gmail.com>
2024-10-17 17:29:03 -0300

    Carousel image y Admin fix

--- Frontend/src/components/Carousel/Carousel.tsx
+++ Frontend/src/components/Carousel/Carousel.tsx
@@ -6,9 +6,9 @@ export default function CarouselComponent() {
     return (
     <div className="w-full h-[550px]">
       <Carousel slideInterval={3000}>
-        <img src="https://i.imgur.com/p9nHrif.png" alt="..." className="object-cover h-full" />
-        <img src="https://i.imgur.com/p9nHrif.png" alt="..." className="object-cover h-full" />
-        <img src="https://i.imgur.com/p9nHrif.png" alt="..." className="object-cover h-full" />
+        <img src="https://i.imgur.com/sCPPGoJ.png" alt="..." className="object-cover h-full" />
+        <img src="https://i.imgur.com/sCPPGoJ.png" alt="..." className="object-cover h-full" />
+        <img src="https://i.imgur.com/sCPPGoJ.png" alt="..." className="object-cover h-full" />
       </Carousel>
     </div>
     )
