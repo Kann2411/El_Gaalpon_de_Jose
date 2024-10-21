5bb5ae530b2b658ed5be9c492ae184d393cee47e
lucasmzd <ilucasmzd@gmail.com>
2024-10-20 23:52:52 -0300

    Appointments y Home view

--- Frontend/src/app/home/page.tsx
+++ Frontend/src/app/home/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import HomeView from '@/views/HomeView/HomeView'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeView />
   )
 }
 
