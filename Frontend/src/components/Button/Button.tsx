a3a9beb873b7a5b2aa30e4d0b3c70bd35d4c4d53
lucasmzd <ilucasmzd@gmail.com>
2024-10-22 15:25:53 -0300

    Fixes y Landing Page

--- Frontend/src/components/Button/Button.tsx
+++ Frontend/src/components/Button/Button.tsx
@@ -16,7 +16,7 @@ const Button: React.FC<IButtonProps> = ({ content, onClick, redirectTo }) => {
 
   return (
     <button
-      className="px-5 py-2 font-bold tracking-wide rounded-2xl bg-red-600 transition duration-300 ease-in-out hover:bg-red-700 text-sm text-white"
+      className="text-lg font-medium px-4 py-2 rounded-md bg-red-600 text-white shadow-md hover:bg-red-800 transition-colors"
       onClick={handleClick}
     >
       {content}
