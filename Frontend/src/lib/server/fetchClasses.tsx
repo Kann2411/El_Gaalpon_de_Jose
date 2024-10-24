ba4704c9a64c009a7d17bf787b57ffcce28b8b08
Julieta <jabadin583@gmail.com>
2024-10-24 13:34:03 -0300

    Clases conectadas a la base de datos

--- Frontend/src/lib/server/fetchClasses.tsx
+++ Frontend/src/lib/server/fetchClasses.tsx
@@ -15,6 +15,34 @@ export const getClassData = async () => {
       console.error('Error fetching class data:', error);
     }
   };
+
+  interface Class {
+    name: string;
+    intensity: string;
+    capacity: number;
+    status: string;
+    image: string;
+    description: string;
+    duration: string;
+    scheludeId:string
+  }
   
 
+  export const createClass = async (classData: Class) => {
+    try {
+      const response = await fetch('http://localhost:3000/class', {
+        method: 'POST',
+        body: JSON.stringify(classData),
+      });
   
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      console.log('Class created:', data);
+      return data;
+    } catch (error) {
+      console.error('Error creating class:', error);
+    }
+  };
