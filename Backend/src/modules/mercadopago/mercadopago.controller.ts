91bd1086577bedfcfeac041a6a0a972915ffe672
jhonatanjcdg <j3172224877@gmail.com>
2024-10-24 22:56:55 -0500

    Token al .env

--- Backend/src/modules/mercadopago/mercadopago.controller.ts
+++ Backend/src/modules/mercadopago/mercadopago.controller.ts
@@ -9,26 +9,26 @@ const client = new MercadoPagoConfig({ accessToken: process.env.ACCESS_TOKEN });
 @Controller('payment')
 export class MercadoPagoController{
     @Post('create_preference')
-  async createPreference(@Body() bodySuscription) {
-    console.log(bodySuscription)
-    const body = {
-      items: [
-        {
-          id: '123',
-          title: bodySuscription.title,
-          quantity: bodySuscription.quantity,
-          unit_price: bodySuscription.price,
-          currency_id: "COL",
+    async createPreference(@Body() bodySuscription) {
+        console.log(bodySuscription)
+        const body = {
+        items: [
+            {
+            id: bodySuscription.id,
+            title: bodySuscription.title,
+            quantity: bodySuscription.quantity,
+            unit_price: bodySuscription.price,
+            currency_id: "COL",
+            }
+        ]
         }
-      ]
+        try {
+        const preference = await new Preference(client).create({body});
+        console.log("preference: ",preference);
+        return {redirectUrl: preference.init_point};
+        } catch (error) {
+        console.log("error: ",error);
+            return error
+        }
     }
-    try {
-      const preference = await new Preference(client).create({body});
-      console.log("preference: ",preference);
-      return {redirectUrl: preference.init_point};
-    } catch (error) {
-      console.log("error: ",error);
-        return error
-    }
-  }
 }
