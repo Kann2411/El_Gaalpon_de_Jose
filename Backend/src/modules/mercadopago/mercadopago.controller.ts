61f4465e10a23cf79af3f21227d346ef4911567b
jhonatanjcdg <j3172224877@gmail.com>
2024-10-25 17:30:21 -0500

    currency_id: 'COL'

--- Backend/src/modules/mercadopago/mercadopago.controller.ts
+++ Backend/src/modules/mercadopago/mercadopago.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, ParseUUIDPipe, Post, Query } from '@nestjs/common';
 import { MercadoPagoService } from './mercadopago.service';
+import { UUID } from 'crypto';
 
 @Controller('mercadopago')
 export class MercadoPagoController {
   constructor(private readonly mercadoPagoService: MercadoPagoService)
   {}
+
+  @Get('status')
+  async getPaymentStatus(@Query('id') id){
+    return this.mercadoPagoService.getPaymentStatus(id)
+  }
 
   @Post('create_preference')
   async createPreference(@Body() bodySuscription) {
